dd6af5ce5842a6dfd599b9de8f103ef4b6497b65
Marguerite MASSON <marguerite.masson@Digital-Grenoble.local>
2022-03-09 13:42:50 +0100

    ProductController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\ProductController;
 
 Route::get("/", [HomeController::class, "homepage"]);
+Route::get("/products", [ProductController::class, "Listeproduits"]);
