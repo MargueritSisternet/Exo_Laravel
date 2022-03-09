d69dd21f27f1835d7f4495277c058c5f8010f0cb
Marguerite MASSON <marguerite.masson@Digital-Grenoble.local>
2022-03-09 15:28:32 +0100

    ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers;
 
 class ProductController extends Controller {
     public function Listeproduits(){
         return('Liste des produits');
     }
-
+    public function Ficheproduit($id) {
+        return('Fiche du produit ' . ($id));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\ProductController;
 
 Route::get("/", [HomeController::class, "homepage"]);
 Route::get("/products", [ProductController::class, "Listeproduits"]);
+Route::get('/product/{id}', [ProductController::class, 'Ficheproduit']);
